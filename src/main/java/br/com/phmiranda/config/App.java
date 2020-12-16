1b2dbf774dd2bb3b91c020771dd93a62705f65eb
pedro.miranda <pedro.miranda@fornecedores.sicoob.com.br>
2020-12-16 18:00:08 -0300

    first commit

---
+++ src/main/java/br/com/phmiranda/config/App.java
@@ -0,0 +1,12 @@
+/**
+ * User: Pedro.Miranda
+ * Project: agenda
+ * Description: this class execute...!
+ * Date: 16/12/2020
+ */
+
+package br.com.phmiranda.config;
+
+public enum App {
+    // comment
+}
---
+++ src/main/java/br/com/phmiranda/repository/AgendaDao.java
@@ -0,0 +1,11 @@
+/**
+ * User: Pedro.Miranda
+ * Project: agenda
+ * Description: this class execute...!
+ * Date: 16/12/2020
+ */
+package br.com.phmiranda.repository;
+
+public class AgendaDao {
+    // comment
+}
